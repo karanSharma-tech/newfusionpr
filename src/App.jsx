22d0fd4bef63ca1d87a7bb56466db6d50b7c3394
devangy <d3vang2002@gmail.com>
2025-08-02 19:34:15 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,36 @@ import React from "react";
 import Navbar from "./component/Navbar.jsx";
 import "./App.css";
 import Layout from "./component/Layout.jsx";
-import About from "./component/about.jsx";
+import About from "./component/About.jsx";
 import Contact from "./component/Contact.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Hero from "./component/Hero.jsx";
 function App() {
-  const router=createBrowserRouter([
-    { path:"/",
-      element:<Layout/>,
-      children:[
+  const router = createBrowserRouter([
     {
-      path:"",
-      element:<Hero/>
-
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Hero />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
     },
-    {
-      path:"/about",
-      element:<About/>
-
-    },
-    {
-path:"/contact",
-element:<Contact/>
-    },]
- } ])
+  ]);
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <RouterProvider router={router}/>
-     
+      <RouterProvider router={router} />
     </>
   );
 }
